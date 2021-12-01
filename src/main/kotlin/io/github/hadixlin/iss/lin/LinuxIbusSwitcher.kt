5395639c2c922dae35282fd0004b2ceefe0d5d54
dzy <shoper2@163.com>
2021-12-01 17:47:04 +0800

    create ibus switcher

---
+++ src/main/kotlin/io/github/hadixlin/iss/lin/LinuxIbusSwitcher.kt
@@ -0,0 +1,17 @@
+package io.github.hadixlin.iss.lin
+
+import io.github.hadixlin.iss.InputMethodSwitcher
+
+class LinuxIbusSwitcher : InputMethodSwitcher {
+    override fun storeCurrentThenSwitchToEnglish() {
+        TODO("Not yet implemented")
+    }
+
+    override fun restore() {
+        TODO("Not yet implemented")
+    }
+
+    override fun switchToEnglish() {
+        TODO("Not yet implemented")
+    }
+}
