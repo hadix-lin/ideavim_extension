5a9e2addf6ae31cdbcc0044723d056143f195dd5
dzy <shoper2@163.com>
2021-12-02 13:48:10 +0800

    输入模块判断

--- src/main/kotlin/io/github/hadixlin/iss/SystemInputMethodSwitcher.kt
+++ src/main/kotlin/io/github/hadixlin/iss/SystemInputMethodSwitcher.kt
@@ -2,6 +2,7 @@ package io.github.hadixlin.iss
 
 import com.maddyhome.idea.vim.ex.vimscript.VimScriptGlobalEnvironment
 import io.github.hadixlin.iss.lin.LinFcitxRemoteSwitcher
+import io.github.hadixlin.iss.lin.LinuxIbusSwitcher
 import io.github.hadixlin.iss.mac.MacInputMethodSwitcher
 import io.github.hadixlin.iss.win.WinInputMethodSwitcher
 import org.apache.commons.lang.SystemUtils
@@ -19,8 +20,20 @@ class SystemInputMethodSwitcher : InputMethodSwitcher {
                 MacInputMethodSwitcher()
             }
         }
-        SystemUtils.IS_OS_LINUX -> LinFcitxRemoteSwitcher()
-        else -> throw IllegalArgumentException("Not Support Current System OS, Only Support Windows, MacOS and Linux(with fcitx)")
+        SystemUtils.IS_OS_LINUX -> {
+//            获取输入法设置的环境变量
+            val qtInputMethod = System.getenv(QT_INPUT_METHOD)
+            val gtkInputMethod = System.getenv(GTK_INPUT_METHOD)
+//            当前系统环境变量判断
+            if (qtInputMethod.equals(INPUT_METHOD_FCITX)||gtkInputMethod.equals(INPUT_METHOD_FCITX) ) {
+                LinFcitxRemoteSwitcher()
+            } else if(qtInputMethod.equals(INPUT_METHOD_IBUS)||gtkInputMethod.equals(INPUT_METHOD_IBUS)) {
+                LinuxIbusSwitcher()
+            }else{
+                throw IllegalArgumentException("Not Support Current Input Method, Only Support Windows, MacOS and Linux(with fcitx and ibus)")
+            }
+        }
+        else -> throw IllegalArgumentException("Not Support Current System OS, Only Support Windows, MacOS and Linux(with fcitx and ibus)")
     }
 
     override fun switchToEnglish() {
@@ -37,5 +50,9 @@ class SystemInputMethodSwitcher : InputMethodSwitcher {
 
     companion object {
         private const val ENGLISH_INPUT_SOURCE_FOR_MAC = "keep_input_source_in_normal"
+        private const val INPUT_METHOD_FCITX="fcitx"
+        private const val INPUT_METHOD_IBUS="ibus"
+        private const val QT_INPUT_METHOD="QT_IM_MODULE"
+        private const val GTK_INPUT_METHOD="GTK_IM_MODULE"
     }
 }
