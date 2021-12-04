1adbb9094b0c495b4b2da708346ecee62097ea8c
邓志宇 <shoper2@163.com>
2021-12-04 15:35:43 +0800

    完成ibus切换，待测试

--- src/main/kotlin/io/github/hadixlin/iss/lin/LinuxIbusSwitcher.kt
+++ src/main/kotlin/io/github/hadixlin/iss/lin/LinuxIbusSwitcher.kt
@@ -1,17 +1,53 @@
 package io.github.hadixlin.iss.lin
 
 import io.github.hadixlin.iss.InputMethodSwitcher
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class LinuxIbusSwitcher : InputMethodSwitcher {
+    private var lastStatus:String = STATUS_ENGLISH;
     override fun storeCurrentThenSwitchToEnglish() {
-        TODO("Not yet implemented")
+        val current = getStatus()
+        if(current == STATUS_ENGLISH){
+            return
+        }
+        lastStatus = current
+        switchToEnglish()
     }
 
     override fun restore() {
-        TODO("Not yet implemented")
+        if (lastStatus!= STATUS_ENGLISH){
+            execSet(lastStatus)
+            lastStatus = STATUS_ENGLISH
+        }
     }
 
     override fun switchToEnglish() {
-        TODO("Not yet implemented")
+        execSet(STATUS_ENGLISH)
+    }
+
+    companion object{
+        private const val STATUS_ENGLISH="xkb:us::eng"
+        private fun execSet(im:String):Process{
+            val cmd = arrayOf("ibus engine",im)
+            val proc = Runtime.getRuntime().exec(cmd)
+            proc.waitFor(3, TimeUnit.SECONDS)
+            return proc
+        }
+
+        private fun getStatus():String{
+            val cmd = "ibus engine"
+            val proc = Runtime.getRuntime().exec(cmd)
+            proc.waitFor(3, TimeUnit.SECONDS)
+            return Scanner(proc.inputStream).use {
+                if (it.hasNextInt()) {
+                    it.nextLine()
+                } else{
+                    ""
+                }
+
+
+            }
+        }
     }
 }
