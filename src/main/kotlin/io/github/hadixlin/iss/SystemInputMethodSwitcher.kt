50ea895b81bfc4abb39a6810ce896fc72f29f158
nobody <nobody@nowhere>
2023-10-30 22:31:54 +0800

    完成自动检测切换 Rime ASCII 模式
    因为使用 dbus 获取当前 Fcitx5 输入法名称只有延迟一定时间才能正确获取，所以 delegate 对象使用的 lazy 初始化。使用 Rime ASCII 模式必须显式声明  *IDEA_VIM_EXTENSION_USE_RIME_ASCII* 环境变量。

--- src/main/kotlin/io/github/hadixlin/iss/SystemInputMethodSwitcher.kt
+++ src/main/kotlin/io/github/hadixlin/iss/SystemInputMethodSwitcher.kt
@@ -2,24 +2,27 @@ package io.github.hadixlin.iss
 
 import com.maddyhome.idea.vim.VimPlugin
 import io.github.hadixlin.iss.lin.LinFcitxRemoteSwitcher
+import io.github.hadixlin.iss.lin.LinFcitx5RimeSwitcher
 import io.github.hadixlin.iss.lin.LinuxIbusSwitcher
 import io.github.hadixlin.iss.mac.MacInputMethodSwitcher
 import io.github.hadixlin.iss.win.WinInputMethodSwitcher
 import org.apache.commons.lang.SystemUtils
+import java.lang.NullPointerException
+import java.util.concurrent.TimeUnit
+import java.util.Scanner
 
 
 /** Created by hadix on 26/12/2018.  */
 class SystemInputMethodSwitcher
 	: InputMethodSwitcher {
-	private var delegate: InputMethodSwitcher
+	private val delegate: InputMethodSwitcher by lazy {
 
-	init {
 		//获取输入法设置的环境变量
 		//当前系统环境变量判断
 		val variableService = VimPlugin.getVariableService()
 		val english = variableService.getGlobalVariableValue(INPUT_SOURCE_IN_NORMAL)
 		val nonEnglish = variableService.getGlobalVariableValue(INPUT_SOURCE_IN_INSERT)
-		delegate = when {
+		return@lazy when {
 			SystemUtils.IS_OS_WINDOWS -> {
 				WinInputMethodSwitcher(
 					english?.toVimNumber()?.value?.toLong(), nonEnglish?.toVimNumber()?.value?.toLong()
@@ -38,9 +41,18 @@ class SystemInputMethodSwitcher
 				//获取输入法设置的环境变量
 				val qtInputMethod = System.getenv(QT_INPUT_METHOD)
 				val gtkInputMethod = System.getenv(GTK_INPUT_METHOD)
+				val useRimeASCII = try {
+					System.getenv(IDEA_VIM_EXTENSION_USE_RIME_ASCII)
+				} catch (e: NullPointerException){
+					""
+				}
 				//当前系统环境变量判断
 				if (isFcitx(qtInputMethod, gtkInputMethod)) {
-					LinFcitxRemoteSwitcher()
+					if (useRimeASCII.isNotEmpty() && canUseRimeAscii()){
+						LinFcitx5RimeSwitcher()
+					}else{
+						LinFcitxRemoteSwitcher()
+					}
 				} else if (isIbus(qtInputMethod, gtkInputMethod)) {
 					LinuxIbusSwitcher()
 				} else {
@@ -73,11 +85,32 @@ class SystemInputMethodSwitcher
 		private const val INPUT_METHOD_IBUS = "ibus"
 		private const val QT_INPUT_METHOD = "QT_IM_MODULE"
 		private const val GTK_INPUT_METHOD = "GTK_IM_MODULE"
+		private const val IDEA_VIM_EXTENSION_USE_RIME_ASCII = "IDEA_VIM_EXTENSION_USE_RIME_ASCII"
+
 		fun isFcitx(qtInputMethod: String?, gtkInputMethod: String?): Boolean {
 			return qtInputMethod == INPUT_METHOD_FCITX
 					|| gtkInputMethod == INPUT_METHOD_FCITX
 					|| qtInputMethod == INPUT_METHOD_FCITX5
 					|| gtkInputMethod == INPUT_METHOD_FCITX5
+		}
+
+		fun canUseRimeAscii(): Boolean{
+			val cmd = arrayOf("busctl", "--user",  "call", "org.fcitx.Fcitx5", "/controller",
+				"org.fcitx.Fcitx.Controller1","CurrentInputMethod")
+			val proc = Runtime.getRuntime().exec(cmd)
+			proc.waitFor(3, TimeUnit.SECONDS)
+			return Scanner(proc.inputStream).use {
+				if (it.hasNext()) {
+					val firstToken = it.next()
+					val sb = StringBuffer()
+					while(it.hasNext()){
+						sb.append(it.next())
+					}
+					firstToken == "s" && sb.contains("rime")
+				} else {
+					false
+				}
+			}
 		}
 
 		fun isIbus(qtInputMethod: String?, gtkInputMethod: String?): Boolean =
